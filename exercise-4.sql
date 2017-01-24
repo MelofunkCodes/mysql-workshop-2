ab5ca11539946b1b18554438b9d84ea4cd2524de
MelofunkCodes <myeoh27@gmail.com>
2017-01-24 19:59:33 +0000

    Added exercises 4, 5, and 6

---
+++ exercise-4.sql
@@ -0,0 +1,5 @@
+UPDATE Account SET password ="h3ll0kitty" where id = 1;
+
+UPDATE AddressBook SET name ="Oui Lobortis Quis Corp." where id = 1;
+
+UPDATE Entry SET contentLineOne = "likes pizza" WHERE id IN (1, 2, 3);
